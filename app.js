2419ee4883fc687c7b0c7c4012c0a3a2b99b45cd
Katelyn Stamas <cakelyn@gmail.com>
2017-10-25 16:47:01 -0400

    completed

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-var GroceryList = () => (
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bold: false
+    };
+  }
+
+  onItemMouseOver() {
+    this.setState ({
+      bold: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : '200'
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onItemMouseOver.bind(this)}> {this.props.item} </li>
+    )
+  }
+}
+
+
+var GroceryList = (props) => (
   <ul>
-    <li>Bread</li>
-    <li>Peanut Butter</li>
+    {props.items.map(item =>
+      <GroceryListItem item={item}/>
+    )}
   </ul>
 );
 
-ReactDOM.render(<GroceryList/>, document.getElementById("app"));
+ReactDOM.render(<GroceryList items={['Bread', 'Peanut butter']} />, document.getElementById("app"));
